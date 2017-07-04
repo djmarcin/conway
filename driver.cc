4816c0989ff7bd7a261a1cc1a3b65cea381a0e40
David Marcin <djmarcin@gmail.com>
2017-07-04 10:56:32 -0700

    Fix rendering when zoomed near the edges of the int64 range.

--- driver.cc
+++ driver.cc
@@ -13,28 +13,31 @@ static bool paused = true;
 static std::unique_ptr<conway::Life> life;
 static int scaleFactor = 8;
 static int delay_ms = 100;
-static std::pair<double, double> viewport_center{0, 0};
+static std::pair<int64_t, int64_t> viewport_center{0, 0};
+static std::pair<int64_t, int64_t> x_range{-1L << scaleFactor, 1L << scaleFactor};
+static std::pair<int64_t, int64_t> y_range{-1L << scaleFactor, 1L << scaleFactor};
 
 void displayCallback() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    std::pair<double, double> x_range = std::make_pair(viewport_center.first - pow(2, scaleFactor - 1), viewport_center.first + pow(2, scaleFactor - 1));
-    std::pair<double, double> y_range = std::make_pair(viewport_center.second - pow(2, scaleFactor - 1), viewport_center.second + pow(2, scaleFactor - 1));
-    gluOrtho2D(x_range.first, x_range.second, y_range.first, y_range.second);
+    gluOrtho2D(-1, 1, -1, 1);
 
+    // Display goes wild at the edges of the int64 space so try to downsample to 2^16
+    int downscale = std::max(0, scaleFactor - 16);
+    int window_dim = 1 << (scaleFactor - 1 - downscale);
     glBegin(GL_POINTS);
     glColor4f(1.0, 1.0, 1.0, 1.0);
     for (const auto& p : life->LivePoints()) {
-        glVertex2d(p.x, p.y);
+        glVertex2d((double)((p.x - viewport_center.first) >> downscale) / window_dim,
+                   (double)((p.y - viewport_center.second) >> downscale) / window_dim);
     }
     glEnd();
 
-    glRasterPos2d(x_range.first + 0.02 * (viewport_center.first - x_range.first),
-                  y_range.second - 0.05 * (y_range.second - viewport_center.second));
+    glRasterPos2d(-0.95, 0.9);
     char buf[512];
-    sprintf(buf, "Generation: %lld - Scale: 2^%d [(%lf, %lf), (%lf, %lf)] - Delay: %dms",
-            life->generation(), scaleFactor, x_range.first, y_range.second, x_range.second, y_range.first, delay_ms);
+    sprintf(buf, "Generation: %lld - Delay: %dms - Scale: 2^%d [(%lld, %lld), (%lld, %lld)]",
+            life->generation(), delay_ms, scaleFactor, x_range.first, y_range.second, x_range.second, y_range.first);
     for (char c : buf) {
         if (c == '\0') { break; }
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, c);
@@ -65,31 +68,47 @@ void PrintLivePoints() {
     }
 }
 
+void correctZoom() {
+    if (scaleFactor == 64) {
+        viewport_center = std::make_pair(0,0);
+        x_range = std::make_pair(std::numeric_limits<int64_t>::min(), std::numeric_limits<int64_t>::max());
+        y_range = std::make_pair(std::numeric_limits<int64_t>::min(), std::numeric_limits<int64_t>::max());
+    } else {
+        int64_t window_dim = 1L << (scaleFactor - 1);
+        viewport_center.first = std::max(viewport_center.first, std::numeric_limits<int64_t>::min() + window_dim);
+        viewport_center.first = std::min(viewport_center.first, std::numeric_limits<int64_t>::max() - window_dim);
+        viewport_center.second = std::max(viewport_center.second, std::numeric_limits<int64_t>::min() + window_dim);
+        viewport_center.second = std::min(viewport_center.second, std::numeric_limits<int64_t>::max() - window_dim);
+        x_range = std::make_pair(viewport_center.first - window_dim, viewport_center.first + window_dim);
+        y_range = std::make_pair(viewport_center.second - window_dim, viewport_center.second + window_dim);
+    }
+}
+
 void keyCallback(unsigned char key, int x, int y) {
     switch (key) {
         case '+':
             scaleFactor = std::max(8, scaleFactor - 1);
-            // correctZoom();
+            correctZoom();
             break;
         case '-':
             scaleFactor = std::min(64, scaleFactor + 1);
-            // correctZoom();
+            correctZoom();
             break;
         case 'w':
             viewport_center.second += 1L << (scaleFactor - 3);
-            // correctZoom();
+            correctZoom();
             break;
         case 'a':
             viewport_center.first -= 1L << (scaleFactor - 3);
-            // correctZoom();
+            correctZoom();
             break;
         case 's':
             viewport_center.second -= 1L << (scaleFactor - 3);
-            // correctZoom();
+            correctZoom();
             break;
         case 'd':
             viewport_center.first += 1L << (scaleFactor - 3);
-            // correctZoom();
+            correctZoom();
             break;
         case '{':
             delay_ms += 10;
